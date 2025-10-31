91677b1fe69f55f81a46bd2280746121f84367f0
oscar-wilson <oscar.wilson8@education.nsw.gov.au>
2025-10-31 12:39:38 +1100

    Task 2 - Activity 3 or 4 I think
    added genre column and movies to database

--- app.py
+++ app.py
@@ -14,6 +14,45 @@ CREATE TABLE IF NOT EXISTS movies (
 )
 ''')
 
-# Commit the changes and close the connection
+cursor.execute("""
+ALTER TABLE movies ADD COLUMN genre TEXT               
+""")
 conn.commit()
+
+# -------------------------------------------------------- Inserting Stuff --------------------------------------------------------
+# Insert Mean Girls (2004)
+cursor.execute("""
+INSERT INTO movies (title, director, year, rating)
+VALUES (?, ?, ?, ?)    
+""", ('Mean Girls', 'Mark Waters', 2004, 7.1))
+
+# Insert multiple musicals
+musicals = [
+    ('Mean Girls', 'Samantha Jayne, Arturo Perez Jr', 2024, 5.5),
+    ('Wicked: Part 1', 'Jon M. Chu', 2024, 7.4),
+    ('Frozen', 'Chris Buck, Jennifer Lee', 2013, 7.4),
+    ('Little Shop of Horrors', 'Frank Oz', 1986, 7.1)
+]
+
+cursor.executemany("""
+INSERT INTO movies (title, director, year, rating)
+VALUES (?, ?, ?, ?)
+""", musicals)
+
+# -------------------------------------------------------- Selecting Stuff --------------------------------------------------------
+conn.commit()
+
+# View all of the movies (because the SQLite viewer extension doesn't work)
+cursor.execute('''
+SELECT * FROM movies;               
+''')
+all_movies = cursor.fetchall()
+print("Selected Movies:")
+for item in all_movies:
+    print(f"\t{item}")
+
+# View all of the musicals (can't do that until I add a genre!)
+
+# -------------------------------------------------------- Finishing Stuff --------------------------------------------------------
+# Commit the changes and close the connection
 conn.close()
