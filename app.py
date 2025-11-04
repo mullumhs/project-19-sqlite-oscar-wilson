7a8596e5ee1c19ce40aef6f2d80a77825990a712
oscar-wilson <oscar.wilson8@education.nsw.gov.au>
2025-11-04 14:10:15 +1100

    Task 2 Final Activity Completed

--- app.py
+++ app.py
@@ -11,6 +11,25 @@ def menu_int(message, choices):
         except:
             print("Error! Invalid Choice!")
 
+def get_rating(message):
+    while True:
+        try:
+            num = float(input(message).strip())
+            if num >= 0 and num <= 10:
+                return num
+            else:
+                raise Exception
+        except:
+            print("Error! Enter a rating between 0-10!")
+
+def get_int(message):
+    while True:
+        try:
+            num = int(input(message).strip())
+            return num
+        except:
+            print("Error! Enter a whole number!")
+
 def create_connection():
     # Create a connection to my_movies.db
     conn = sqlite3.connect("my_movies.db")
@@ -55,41 +74,22 @@ def display_all_movies(conn):
     selected_movies = cursor.fetchall()
 
     # Display all movies
-    print("\nMy Movies:")
+    print("My Movies:")
     for movie in selected_movies:
         print(f"ID: {movie[0]} - {movie[1]}\n\t-\tDirector(s): {movie[2]}\n\t-\tYear: {movie[3]}\n\t-\tRating: {movie[4]}/10\n\t-\tGenre: {movie[5]}".expandtabs(2))
-    print()
 
-def update_movie_rating(conn, title, new_rating):
-    # Select movies by title
+def update_movie_rating(conn, title):
+    # Select movie id by title
     cursor = conn.cursor()
-
-    cursor.execute(f"""
-    SELECT * FROM my_movies WHERE title = "{title}"
-    """)
-
-    selected_movies = cursor.fetchall()
-
-    # Check if only one movie is selected
-    if len(selected_movies) == 1:
-        selected_id = selected_movies[0][0]
-
-    elif len(selected_movies) > 1:
-        # Get a list of all the ids that have been selected
-        id_choices = [movie[0] for movie in selected_movies]
-
-        # Display all of the selected movies
-        print(f"\nMovies named {title}:")
-        for movie in selected_movies:
-            print(f"ID: {movie[0]} - {movie[1]}\n\t-\tDirector(s): {movie[2]}\n\t-\tYear: {movie[3]}\n\t-\tRating: {movie[4]}/10\n\t-\tGenre: {movie[5]}".expandtabs(2))
-        print()
-
-        selected_id = menu_int(message=f"Select the correct movie ID and enter your choice: ", choices=id_choices)
-
-    elif len(selected_movies) == 0:
-        print(f"Error! No movies titled {title}")
-        return
     
+    try:
+        selected_id = select_id_by_title(conn, title)
+    except:
+        return
+
+    # Get new rating
+    new_rating = get_rating("Enter new rating (0-10): ")
+
     # Update based on selected movie id
     cursor.execute(f"""
     UPDATE my_movies SET rating = {new_rating} WHERE id = {selected_id}
@@ -98,8 +98,21 @@ def update_movie_rating(conn, title, new_rating):
     conn.commit()
 
 def delete_movie(conn, title):
-    # TODO: Delete a specified movie from the database
-    pass
+    # ------------- Delete a specified movie from the database
+    # Select movies by title
+    cursor = conn.cursor()
+    
+    try:
+        selected_id = select_id_by_title(conn, title)
+    except:
+        return
+    
+    # Delete based on selected movie id
+    cursor.execute(f"""
+    DELETE FROM my_movies WHERE id = {id}
+    """)
+
+    conn.commit()
 
 def find_movies_by_director(conn, director):
     # Select movies by director
@@ -112,10 +125,46 @@ def find_movies_by_director(conn, director):
     selected_movies = cursor.fetchall()
 
     # Display all movies by director
-    print(f"\nMovies by {director}:")
+    print(f"Movies by {director}:")
     for movie in selected_movies:
         print(f"ID: {movie[0]} - {movie[1]}\n\t-\tDirector(s): {movie[2]}\n\t-\tYear: {movie[3]}\n\t-\tRating: {movie[4]}/10\n\t-\tGenre: {movie[5]}".expandtabs(2))
-    print()
+
+def select_id_by_title(conn, title):
+    # Select movie id by title
+    cursor = conn.cursor()
+
+    cursor.execute(f"""
+    SELECT * FROM my_movies WHERE title = "{title}"
+    """)
+
+    selected_movies = cursor.fetchall()
+
+    # Check if only one movie is selected
+    if len(selected_movies) == 1:
+        selected_id = selected_movies[0][0]
+        return selected_id
+
+    elif len(selected_movies) > 1:
+        # Get a list of all the ids that have been selected
+        id_choices = [movie[0] for movie in selected_movies]
+
+        # Display all of the selected movies
+        print(f"Movies named {title}:")
+        for movie in selected_movies:
+            print(f"ID: {movie[0]} - {movie[1]}\n\t-\tDirector(s): {movie[2]}\n\t-\tYear: {movie[3]}\n\t-\tRating: {movie[4]}/10\n\t-\tGenre: {movie[5]}".expandtabs(2))
+        print()
+
+        selected_id = menu_int(message=f"Select the correct movie ID and enter your choice: ", choices=id_choices)
+        return selected_id
+
+    elif len(selected_movies) == 0:
+        print(f"Error! No movies titled {title}")
+        raise Exception
+    
+    else: # This should never happen?
+        print(len(selected_movies))
+        print(selected_movies)
+        raise Exception
 
 def main():
     conn = create_connection()
@@ -132,12 +181,13 @@ def main():
             print("6. Exit")
             
             choice = input("Enter your choice (1-6): ")
+            print()
             
             if choice == '1':
                 title = input("Enter movie title: ").strip()
                 director = input("Enter director name: ").strip()
-                year = int(input("Enter release year: ").strip())
-                rating = float(input("Enter rating (0-10): ").strip())
+                year = get_int("Enter release year: ")
+                rating = get_rating("Enter rating (0-10): ")
                 genre = input("Enter movie genre: ").title().strip()
                 add_movie(conn, title, director, year, rating, genre)
                 print("Movie added successfully!")
@@ -147,8 +197,7 @@ def main():
             
             elif choice == '3':
                 title = input("Enter movie title to update: ")
-                new_rating = float(input("Enter new rating (0-10): "))
-                update_movie_rating(conn, title, new_rating)
+                update_movie_rating(conn, title)
                 print("Rating updated successfully!")
             
             elif choice == '4':
@@ -161,14 +210,16 @@ def main():
                 find_movies_by_director(conn, director)
             
             elif choice == '6':
-                print("Thank you for using Movie Database Manager. Goodbye!")
+                print("Thank you for using Movie Database Manager. Goodbye!\n")
                 break
             
             else:
                 print("Invalid choice. Please try again.")
-        
+
+            input("\nPress Enter to Continue: ")
+
         conn.close()
-        input("Press Enter to Continue: ")
+        
     else:
         print("Error! Cannot create the database connection.")
 
